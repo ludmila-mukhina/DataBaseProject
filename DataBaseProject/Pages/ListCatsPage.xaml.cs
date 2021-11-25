9a1932d5d1442bca27e08a70ceda015ff6a3c0df
lyudmila.archackowa <lyudmila.archackowa@yandex.ru>
2021-11-25 18:56:19 +0300

    Фильтрация и сортировка

--- DataBaseProject/Pages/ListCatsPage.xaml.cs
+++ DataBaseProject/Pages/ListCatsPage.xaml.cs
@@ -20,11 +20,26 @@ namespace DataBaseProject
     /// </summary>
     public partial class ListCatsPage : Page
     {
-        
+        List<Cats> CatsStart = BaseClass.Base.Cats.ToList();
         public ListCatsPage()
         {
             InitializeComponent();
-            LVCats.ItemsSource = BaseClass.Base.Cats.ToList();
+            LVCats.ItemsSource = CatsStart;
+            TblCount.Text = CatsStart.Count + "";
+            //List<Diets> D = BaseClass.Base.Diets.ToList();
+            //int sum = 0;
+            //for (int i=0; i<D.Count(); i++)
+            //{
+            //    sum += D[i].QuantityMonth * D[i].FeedCat.Pricefeed;
+            //}
+            //TblCount.Text = sum + "";
+            List<GenderTable> GB = BaseClass.Base.GenderTable.ToList();
+            CBFilter.Items.Add("Все");
+            for (int i=0; i<GB.Count(); i++)
+            {
+                CBFilter.Items.Add(GB[i].Genger);
+            }
+            CBFilter.SelectedIndex = 0;
         }
 
         private void TextBlock_Loaded(object sender, RoutedEventArgs e)
@@ -76,6 +91,61 @@ namespace DataBaseProject
             Cats CatUpdate = BaseClass.Base.Cats.FirstOrDefault(y => y.idCat == ind);  // находим кота с соответствующим индексом
             FrameClass.FrameMain.Navigate(new CreateOrUpdatePage(CatUpdate));  // переходим на страницу с формой добавления, которую будем использовать и для редактирования
             // Обратите внимание, что конструктор в этом случае не пустой. Он содержит того кота, который соотвествует нужному индексу
+        }
+        List<Cats> CatsFilter;
+        private void Filter()
+        {
+            int index = CBFilter.SelectedIndex;
+            if (index != 0)
+            {
+                CatsFilter = CatsStart.Where(x => x.IDGender == index).ToList();
+            }
+            else
+            {
+                CatsFilter = CatsStart;
+            }
+            if (!string.IsNullOrWhiteSpace(TBFilter.Text))
+            {
+                CatsFilter = CatsFilter.Where(x => x.Breed.ToLower().Contains(TBFilter.Text)).ToList();
+            }
+            if (ChBFilter.IsChecked==true)
+            {
+                CatsFilter = CatsFilter.Where(x => x.CatPhoto != null).ToList();
+            }
+            LVCats.ItemsSource = CatsFilter;
+            TblCount.Text = CatsFilter.Count() + "";
+        }
+        private void CBFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Filter();
+        }
+        
+        private void TBFilter_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Filter();
+        }
+
+        private void ChBFilter_Checked(object sender, RoutedEventArgs e)
+        {
+            Filter();
+        }
+
+        private void ChBFilter_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Filter();
+        }
+
+        private void BtnSortUp_Click(object sender, RoutedEventArgs e)
+        {
+            CatsFilter.Sort((x, y) => x.СatName.CompareTo(y.СatName));
+            LVCats.Items.Refresh();
+        }
+
+        private void BtnSortDown_Click(object sender, RoutedEventArgs e)
+        {
+            CatsFilter.Sort((x, y) => x.СatName.CompareTo(y.СatName));
+            CatsFilter.Reverse();
+            LVCats.Items.Refresh();
         }
     }
 }
