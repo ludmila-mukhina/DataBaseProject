75e7a6ad78a9eef0fc521771e63594d3edc749ae
lyudmila.archackowa <lyudmila.archackowa@yandex.ru>
2021-12-03 13:11:55 +0300

    Пагинация

--- DataBaseProject/Pages/ListCatsPage.xaml.cs
+++ DataBaseProject/Pages/ListCatsPage.xaml.cs
@@ -21,25 +21,20 @@ namespace DataBaseProject
     public partial class ListCatsPage : Page
     {
         List<Cats> CatsStart = BaseClass.Base.Cats.ToList();
+        PageChange pc = new PageChange();  // создаем объект класса для отображения страниц
         public ListCatsPage()
         {
             InitializeComponent();
             LVCats.ItemsSource = CatsStart;
-            TblCount.Text = CatsStart.Count + "";
-            //List<Diets> D = BaseClass.Base.Diets.ToList();
-            //int sum = 0;
-            //for (int i=0; i<D.Count(); i++)
-            //{
-            //    sum += D[i].QuantityMonth * D[i].FeedCat.Pricefeed;
-            //}
-            //TblCount.Text = sum + "";
+            TblCount.Text = CatsStart.Count + "";  // отоборажение исходного количества записей в списке
             List<GenderTable> GB = BaseClass.Base.GenderTable.ToList();
-            CBFilter.Items.Add("Все");
-            for (int i=0; i<GB.Count(); i++)
+            CBFilter.Items.Add("Все");  // добавление в Combobox элемента "Все записи"
+            for (int i=0; i<GB.Count(); i++)  // добавление в элеметов вз списка GB (в котором хранятся сведения о поле)
             {
                 CBFilter.Items.Add(GB[i].Genger);
             }
-            CBFilter.SelectedIndex = 0;
+            CBFilter.SelectedIndex = 0;  // по умолчанию будет выбран в Combobox элемент "Все"
+            DataContext = pc;  // добавляем объект для отображения страниц в ресурсы страницы
         }
 
         private void TextBlock_Loaded(object sender, RoutedEventArgs e)
@@ -92,28 +87,28 @@ namespace DataBaseProject
             FrameClass.FrameMain.Navigate(new CreateOrUpdatePage(CatUpdate));  // переходим на страницу с формой добавления, которую будем использовать и для редактирования
             // Обратите внимание, что конструктор в этом случае не пустой. Он содержит того кота, который соотвествует нужному индексу
         }
-        List<Cats> CatsFilter;
-        private void Filter()
+        List<Cats> CatsFilter;  // список для хранения отфильтрованных данных
+        private void Filter()  // метод для фильтрации по всем введенным параметрам
         {
-            int index = CBFilter.SelectedIndex;
-            if (index != 0)
+            int index = CBFilter.SelectedIndex;  
+            if (index != 0) // определения выбранного элемента из Combobox
             {
                 CatsFilter = CatsStart.Where(x => x.IDGender == index).ToList();
             }
             else
             {
-                CatsFilter = CatsStart;
+                CatsFilter = CatsStart;  // если ни один из эл-тов Combobox не выбран (по умолчанию в этом случае будут все записи)
             }
-            if (!string.IsNullOrWhiteSpace(TBFilter.Text))
+            if (!string.IsNullOrWhiteSpace(TBFilter.Text))  // будет происходить фильтрация только если текстовое поле не пустое или не заполнено пробелами
             {
                 CatsFilter = CatsFilter.Where(x => x.Breed.ToLower().Contains(TBFilter.Text)).ToList();
             }
-            if (ChBFilter.IsChecked==true)
+            if (ChBFilter.IsChecked==true)  //  фильтрация будет осуществляться в зависимости от того, активен флажок, или нет
             {
                 CatsFilter = CatsFilter.Where(x => x.CatPhoto != null).ToList();
             }
-            LVCats.ItemsSource = CatsFilter;
-            TblCount.Text = CatsFilter.Count() + "";
+            LVCats.ItemsSource = CatsFilter;  // отображение нового отфильтрованного списка в шаблоне
+            TblCount.Text = CatsFilter.Count() + "";  // отображения количества записей, найденных в результате фильтрации
         }
         private void CBFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
@@ -135,17 +130,51 @@ namespace DataBaseProject
             Filter();
         }
 
-        private void BtnSortUp_Click(object sender, RoutedEventArgs e)
+        private void BtnSortUp_Click(object sender, RoutedEventArgs e)  // сортировка по возрастанию
         {
             CatsFilter.Sort((x, y) => x.СatName.CompareTo(y.СatName));
             LVCats.Items.Refresh();
         }
 
-        private void BtnSortDown_Click(object sender, RoutedEventArgs e)
+        private void BtnSortDown_Click(object sender, RoutedEventArgs e)  // сортировка по убыванию
         {
             CatsFilter.Sort((x, y) => x.СatName.CompareTo(y.СatName));
             CatsFilter.Reverse();
             LVCats.Items.Refresh();
+        }
+
+        private void txtPageCount_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                pc.CountPage = Convert.ToInt32(txtPageCount.Text); // если в текстовом поле есnь значение, присваиваем его свойству объекта, которое хранит количество записей на странице
+            }
+            catch
+            {
+                pc.CountPage = CatsFilter.Count; // если в текстовом поле значения нет, присваиваем свойству объекта, которое хранит количество записей на странице количество элементов в списке
+            }
+            pc.Countlist = CatsFilter.Count;  // присваиваем новое значение свойству, которое в объекте отвечает за общее количество записей
+            LVCats.ItemsSource = CatsFilter.Skip(0).Take(pc.CountPage).ToList();  // отображаем первые записи в том количестве, которое равно CountPage
+        }
+
+        private void GoPage_MouseDown(object sender, MouseButtonEventArgs e)  // обработка нажатия на один из Textblock в меню с номерами страниц
+        {
+            TextBlock tb = (TextBlock)sender;
+            switch (tb.Uid)  // определяем, куда конкретно было сделано нажатие
+            {
+                case "prev":
+                    pc.CurrentPage--;
+                    break;
+                case "next":
+                    pc.CurrentPage++;
+                    break;
+                default:
+                    pc.CurrentPage = Convert.ToInt32(tb.Text);
+                    break;
+            }
+            LVCats.ItemsSource = CatsFilter.Skip(pc.CurrentPage*pc.CountPage-pc.CountPage).Take(pc.CountPage).ToList();  // оображение записей постранично с определенным количеством на каждой странице
+            // Skip(pc.CurrentPage* pc.CountPage - pc.CountPage) - сколько пропускаем записей
+            // Take(pc.CountPage) - сколько записей отображаем на странице
         }
     }
 }
